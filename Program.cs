94d6a7295679b4b5ed3c38b08e65e981e2a1dd66
Dwjhdajfhawfwa <185530119+Dwjhdajfhawfwa@users.noreply.github.com>
2024-10-18 20:33:36 +0200

    Center text.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using game;
+using Raylib_cs;
 
-namespace HelloWorld;
+namespace game;
 
 class Program
 {
     public static void Main()
     {
-        Raylib.InitWindow(800, 480, "Hello World");
+        
+        // Resizeable, make sure to adjust every texture
+        Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
+        Raylib.InitWindow(GlobalVars.screenWidth, GlobalVars.screenHeight, "EliteLegends");
+
+        // 240 hz monitor shrug
+        Raylib.SetTargetFPS(60);
 
         while (!Raylib.WindowShouldClose())
         {
@@ -14,9 +22,24 @@ class Program
             Raylib.ClearBackground(Color.White);
             Raylib.ClearBackground(Color.Red);
 
-            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            string text = "Hello world!";
+            int center = (GlobalVars.screenWidth / 2);
+            Vector2 textSize = Raylib.MeasureTextEx(Raylib.GetFontDefault(), text, 20, 0);
+
+            Raylib.DrawText(text, (int) ((GlobalVars.screenWidth / 2) - (textSize.Length() / 2)), 12, 20, Color.Black);
+
+            // Wanted to draw a line under, but couldn't figure out the cords.
+            // It's something along the lines of text start cord to text end cord, and a y cord of some type.
 
             Raylib.EndDrawing();
+
+            if (Raylib.IsWindowResized())
+            {
+                GlobalVars.screenWidth = Raylib.GetScreenWidth();
+                GlobalVars.screenHeight = Raylib.GetScreenHeight();
+
+            }
+
         }
 
         Raylib.CloseWindow();
